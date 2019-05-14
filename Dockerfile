7b2d50933beed9c5a16073ae7a543ea55c8ed947
guzman-raphael <38401847+guzman-raphael@users.noreply.github.com>
2019-05-14 15:24:51 -0500

    Revert "Add testing users and privileges for unit test support."

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
 FROM mysql:5.7
 
-MAINTAINER Raphael Guzman
+MAINTAINER Edgar Y. Walker
 
 ADD my.cnf /etc/mysql/my.cnf
-ADD init.sql /docker-entrypoint-initdb.d/
--- init.sql
+++
@@ -1,6 +0,0 @@
-
-CREATE USER 'datajoint'@'%' IDENTIFIED BY 'datajoint';
-GRANT ALL PRIVILEGES ON `djtest%`.* TO 'datajoint'@'%';
-
-CREATE USER 'djview'@'%' IDENTIFIED BY 'djview';
-grant select on `djtest%`.* to 'djview'@'%';
