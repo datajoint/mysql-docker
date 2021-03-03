bc96ffe359fb97a5fd81250a5998c914d074162c
guzman-raphael <raphael.h.guzman@gmail.com>
2021-03-03 12:03:19 -0600

    Remove sphinx docs for now and clean up.

--- utilities/datajoint-entrypoint.sh
+++ utilities/datajoint-entrypoint.sh
@@ -15,8 +15,9 @@ if echo "${DATAJOINT_DATABASE_CONFIG_MONITOR}" | grep -i true &>/dev/null; then
 		CURR_LAST_MOD_TIME=$(date -r /etc/mysql/my.cnf +%s)
 		CURR_DELTA=$(expr $CURR_LAST_MOD_TIME - $INIT_TIME)
 		if [ "$DELTA" -lt "$CURR_DELTA" ]; then
-			echo "[$(date -u '+%Y-%m-%d %H:%M:%S')][DataJoint]: Config Update: Reloading \
-				MySQL since \`$CURR_FILEPATH\` changed." | tr -d '\n' | tr -d '\t'
+			echo "[$(date -u '+%Y-%m-%d %H:%M:%S')][DataJoint]: Config Update: \
+				Reloading MySQL since \`$CURR_FILEPATH\` changed." | \
+				tr -d '\n' | tr -d '\t'
 			kill "${MYSQL_PID}"
 			/usr/local/bin/docker-entrypoint.sh "$@" &
 			MYSQL_PID=$!
@@ -26,5 +27,5 @@ if echo "${DATAJOINT_DATABASE_CONFIG_MONITOR}" | grep -i true &>/dev/null; then
 		fi
 	done
 else
-    tail -f /dev/null
+	tail -f /dev/null
 fi
